342a6973db8701b1824dde39a42584cac8239a3a
Jorge Ros Gil <jorrgme@gmail.com>
2015-03-03 19:38:21 +0100

    Second commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
@@ -12,12 +13,25 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			primaryStage.initStyle(StageStyle.UNDECORATED);
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/ventana.fxml"));
 			Scene scene = new Scene(root,500,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Manejo de eventos");
 			primaryStage.show();
+			scene.setOnMousePressed((e)->{
+                double x1 = primaryStage.getX() - e.getScreenX();
+                double y1 = primaryStage.getY() - e.getScreenY();
+             
+            scene.setOnMouseDragged((a)->{
+                double x = a.getScreenX()+x1;
+                double y = a.getScreenY()+y1;
+                primaryStage.setX(x);
+                primaryStage.setY(y);
+                });
+             
+            });
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- src/controller/controlador.java
+++ src/controller/controlador.java
@@ -1,7 +1,10 @@
 package controller;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -18,20 +21,37 @@ public class controlador {
 	Button botCerrar;
 	@FXML
 	AnchorPane ap;
+	@FXML 
+	Label etiquetapos;
+	
+	private IntegerProperty x = new SimpleIntegerProperty();
+	private IntegerProperty y = new SimpleIntegerProperty();
 	
 	@FXML
-	protected void desplazar(KeyEvent event){
+	protected void desplazar(KeyEvent event){	
 		if(event.getCode()==KeyCode.UP && gp.getRowIndex(boton)>0){
 			gp.setRowIndex(boton, gp.getRowIndex(boton)-1);
+			x.setValue(gp.getRowIndex(boton));
+			y.setValue(gp.getColumnIndex(boton));
+			boton.setText("("+x.getValue()+","+y.getValue()+")");
 		}
 		if(event.getCode()==KeyCode.DOWN && gp.getRowIndex(boton)<4){
 			gp.setRowIndex(boton, gp.getRowIndex(boton)+1);
+			x.setValue(gp.getRowIndex(boton));
+			y.setValue(gp.getColumnIndex(boton));
+			boton.setText("("+x.getValue()+","+y.getValue()+")");
 		}
 		if(event.getCode()==KeyCode.RIGHT && gp.getColumnIndex(boton)<4){
 			gp.setColumnIndex(boton, gp.getColumnIndex(boton)+1);
+			x.setValue(gp.getRowIndex(boton));
+			y.setValue(gp.getColumnIndex(boton));
+			boton.setText("("+x.getValue()+","+y.getValue()+")");
 		}
 		if(event.getCode()==KeyCode.LEFT && gp.getColumnIndex(boton)>0){
 			gp.setColumnIndex(boton, gp.getColumnIndex(boton)-1);
+			x.setValue(gp.getRowIndex(boton));
+			y.setValue(gp.getColumnIndex(boton));
+			boton.setText("("+x.getValue()+","+y.getValue()+")");
 		}
 	}
 	
@@ -40,5 +60,6 @@ public class controlador {
 		Stage stage = (Stage) botCerrar.getScene().getWindow();
 		stage.close();
 	}	
+
 	
 }
